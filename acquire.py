58888a685b035dd597e86fc171bc6b13d00a8264
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-15 22:02:51 -0500

    Complete functions

---
+++ acquire.py
@@ -0,0 +1,57 @@
+from os import path
+import time
+import requests
+
+import pandas as pd
+
+def is_too_old(filepath):
+    file_age = time.time() - path.getmtime(filepath)
+
+    seconds = file_age
+    minutes = int(seconds) / 60
+    hours = minutes / 60
+    days = hours / 24
+    
+    return days > 1
+
+def file_not_found(filepath):
+    return not path.isfile(filepath)
+
+def get_red_wine_data():
+    filename = 'red_wine_quality.csv'
+    
+    if file_not_found(filename):
+        red = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', sep=';')
+        red.to_csv(filename)
+        
+    elif is_too_old(filename):
+        red = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', sep=';')
+        red.to_csv(filename)
+        
+    return pd.read_csv(filename)
+
+def get_white_wine_data():
+    filename = 'white_wine_quality.csv'
+    
+    if file_not_found(filename):
+        red = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep=';')
+        red.to_csv(filename)
+    
+    elif is_too_old(filename):
+        red = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep=';')
+        red.to_csv(filename)
+        
+    return pd.read_csv(filename)
+
+def get_wine_names_file():
+    filename = 'winequality.names'
+    
+    if file_not_found(filename):
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality.names'
+        myfile = requests.get(url)
+        open('./winequality.names', 'wb').write(myfile.content)
+    
+    elif is_too_old(filename):
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality.names'
+        myfile = requests.get(url)
+        open('./winequality.names', 'wb').write(myfile.content)
